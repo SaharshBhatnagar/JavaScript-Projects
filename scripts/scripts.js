bae3cdbdbf3b6853fc3bce6f92d46c56cd26a6c3
Saharsh Bhatnagar <saharsh.cse@gmail.com>
2026-01-31 21:47:32 +0530

    added js for opening and closing of form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
+const openForm = document.querySelector('.Feedback-icon');
+const closeForm = document.querySelector('.exit');
+const feedbackForm = document.querySelector('.Feedback-block');
+const submitBtn = document.querySelector('.submit-feedback');
+const ratingBtn = document.querySelector('.star');
 
+// open
+openForm.addEventListener('click', () => {
+    feedbackForm.style.display = 'block';
+});
+
+// close
+closeForm.addEventListener('click', () => {
+    feedbackForm.style.display = 'none';
+});
