fd6850b543ea67f1256cfe6da607b97527614828
Saharsh Bhatnagar <saharsh.cse@gmail.com>
2026-02-02 22:09:01 +0530

    intial structure and Design

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
+const canva = document.getElementsByClassName('.drawZone');
 
+const convaRender = canva.getContext('2d');
+
+canva.width = window.innerWidth;
+canva.height = window.innerHeight;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+// Set the brush style
+ctx.strokeStyle = '#000000'; // Black color
+ctx.lineWidth = 5;           // Thickness
+ctx.lineCap = 'round';       // Smooth ends
+ctx.lineJoin = 'round';      // Smooth corners
