2d02ea1f254f70b8d48c8a7ee5b99f0dc274ba6b
Saharsh Bhatnagar <saharsh.cse@gmail.com>
2026-02-25 03:55:38 +0530

    added calender days fetching grid

--- MonthCalender/Scripts/scripts.js
+++ MonthCalender/Scripts/scripts.js
@@ -1,1 +1,79 @@
+const btnPrev = document.querySelector('#previous');
+const btnNext = document.querySelector('#Next');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
+const days = document.querySelector('.days');
+const svgColorContainer = document.querySelector('#svg-color-container');
 
+const now = new Date();
+
+const currentYear = now.getFullYear();
+const currentMonth = now.getMonth() + 1; // Months are 0-11
+const currentDate = now.getDate();
+
+
+let activeYear = currentYear;
+let activeMonth = currentMonth;
+
+let selectedDate = currentDate;
+
+const Months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+const monthClasses = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+
+function renderCalender() {
+
+    month.innerHTML = Months[activeMonth - 1];
+    year.innerHTML = activeYear;
+
+    svgColorContainer.className = `watercolor-bg ${monthClasses[activeMonth - 1]}`;
+
+    const firstDaySnapshot = new Date(activeYear, activeMonth - 1, 1);
+    const firstDayOffset = firstDaySnapshot.getDay(); // weekdays 0-6
+
+
+    const dateSetup = new Date(activeYear, activeMonth, 0)
+    const TotalDays = dateSetup.getDate();
+
+    // Setup grid days for each month
+    days.innerHTML = '';
+
+    for(let i = 0; i < firstDayOffset; i++) {
+        days.insertAdjacentHTML('beforeend', '<div class="empty"> </div>');
+    };
+
+    // adding days
+    for (let i = 0; i < TotalDays; i++) {
+        let isToday = "";
+
+        // Highlight Current date
+        if (i + 1 === currentDate && currentMonth === activeMonth && currentYear === activeYear) {
+            isToday = "today";
+        };
+
+        days.insertAdjacentHTML('beforeend', `<div class="day ${isToday}">${i + 1}</div>`);
+    };
+};
+
+renderCalender();
+
+btnNext.addEventListener('click', () => {
+    activeMonth += 1;
+    if (activeMonth > 12) {
+        activeMonth = 1;
+        activeYear += 1
+    }
+    renderCalender();
+});
+
+btnPrev.addEventListener('click', () => {
+    activeMonth -= 1;
+    if (activeMonth < 1) {
+        activeMonth = 12;
+        activeYear -= 1
+    }
+    
+    renderCalender();
+});
